d33c18bc084cd9b604b73260c09d551a1525ffbf
Matteo Centenaro <bugant@gmail.com>
2016-10-26 09:36:41 +0200

    Minor fixes

--- addon/components/tree-options.js
+++ addon/components/tree-options.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
 import layout from '../templates/components/tree-options';
-import OptionsComponent from 'ember-power-select/components/power-select/options';
 
 export default Ember.Component.extend({
   layout,
