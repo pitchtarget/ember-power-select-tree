f7b6a086780ee7fa8eea924d8d71af5fa7b190d1
bugman <onlyforwin@gmail.com>
2016-11-14 12:52:57 +0100

    NEW: add sass env, with main and variables files created

--- addon/components/tree-options.js
+++ addon/components/tree-options.js
@@ -5,6 +5,8 @@ const { get, Component } = Ember;
 
 export default Component.extend({
   layout,
+  tagName: 'ul',
+  classNames: ['ember-power-select-tree-list'],
   actions: {
     onToggleGroup(node) {
       get(this, 'onToggleGroup')(node);
