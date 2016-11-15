fe78342f42c41726a9823135a097c4d8e999cb2c
bugman <onlyforwin@gmail.com>
2016-11-15 18:09:24 +0100

    NEW: add blank unit test

---
+++ tests/unit/components/ember-power-select-tree-test.js
@@ -0,0 +1,9 @@
+import { moduleForComponent, test } from 'ember-qunit';
+
+moduleForComponent('ember-power-select-tree', 'Unit | Component | ember power select tree', {
+  unit: true
+});
+
+test('it renders', function(assert) {
+  this.subject();
+});
